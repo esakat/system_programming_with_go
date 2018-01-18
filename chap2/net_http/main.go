011d516500996882bcbd9307cd97f10300f57b03
esakat <esaka.tom@gmail.com>
2018-01-19 08:59:09 +0900

    modify memo

--- chap2/net_http/main.go
+++ chap2/net_http/main.go
@@ -8,6 +8,8 @@ import (
 /**
 	net/httpのRequest構造体は、用途の限定されたio.Writer実装メソッドを持つ
 	HTTPリクエストを扱う際の構造体だよ
+	2.4.4のHTTP手書きプロトコルより、こっちの方がミス減るしおすすめ
+
 **/
 func main() {
 	request, err := http.NewRequest("GET", "http://ascii.jp", nil)
