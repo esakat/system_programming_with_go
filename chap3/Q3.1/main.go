aa8897f524abe8c80ae048b70ce8569fe75006c5
esakat <esaka.tom@gmail.com>
2018-01-24 08:29:16 +0900

    finish Q3.1

---
+++ chap3/Q3.1/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+func main() {
+	// 読み込み用のファイルを開く
+	oldfile, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	defer oldfile.Close()
+
+	// 書き込み用のファイルを開く
+	// 新規作成用にこちらはos.Create()を使う
+	newfile, err := os.Create(os.Args[2])
+	if err != nil {
+		panic(err)
+	}
+	defer newfile.Close()
+
+	io.Copy(newfile, oldfile)
+}
+
+/*
+第１引数に読み込みファイル、第２ファイルに出力するファイル名を指定してください
+$ go run main.go old.txt new.txt
+*/
