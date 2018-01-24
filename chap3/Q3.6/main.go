8e16860196054933be07c4fff9899bb5adcb2f33
esakat <esaka.tom@gmail.com>
2018-01-24 09:35:07 +0900

    add Q3.6 template

---
+++ chap3/Q3.6/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+var (
+	computer    = strings.NewReader("COMPUTER")
+	system      = strings.NewReader("SYSTEM")
+	programming = strings.NewReader("PROGRAMMING")
+)
+
+func main() {
+	var stream io.Reader
+
+	io.Copy(os.Stdout, stream)
+}
