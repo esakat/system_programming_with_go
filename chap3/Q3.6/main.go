58bef8a669e84319c7b277267d455e1198898864
esakat <esaka.tom@gmail.com>
2018-01-24 20:04:38 +0900

    finish chap3

--- chap3/Q3.6/main.go
+++ chap3/Q3.6/main.go
@@ -15,5 +15,22 @@ var (
 func main() {
 	var stream io.Reader
 
+	/*
+		ここにioパッケージを使ったコードをかく
+	*/
+	stream = io.MultiReader(
+		// SectionReaderで指定した箇所から１バイトを読み込み
+		io.NewSectionReader(programming, 5, 1), // A
+		io.NewSectionReader(system, 0, 1),      // S
+		io.NewSectionReader(computer, 0, 1),    // C
+		io.NewSectionReader(programming, 8, 1), // I
+		io.NewSectionReader(programming, 8, 1), // I
+	)
+
 	io.Copy(os.Stdout, stream)
 }
+
+/**
+$ go run main.go
+ASCII
+**/
