e853b5ec27bb24d8bf27f4e80212334c05eee427
esakat <esaka.tom@gmail.com>
2018-02-24 21:00:36 +0900

    add benchmark

---
+++ chap8/benchmark/tcpserver.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+)
+
+func TCPServer() {
+	listener, err := net.Listen("tcp", "localhost:18888")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		// リクエストを受け付けたら、実行される
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		// 非同期で処理を行う
+		go func() {
+			// リクエスト読み込み
+			request, err := http.ReadRequest(
+				bufio.NewReader(conn))
+			if err != nil {
+				panic(err)
+			}
+			// リクエストの取り出し
+			_, err = httputil.DumpRequest(request, true)
+			if err != nil {
+				panic(err)
+			}
+			// レスポンスを作成
+			response := http.Response{
+				StatusCode: 200,
+				ProtoMajor: 1,
+				ProtoMinor: 0,
+				Body: ioutil.NopCloser(
+					strings.NewReader("Hello, World!\n"),
+				),
+			}
+			// レスポンス書き込み
+			response.Write(conn)
+			conn.Close()
+		}()
+	}
+}
---
+++ chap8/benchmark/unixdomainsocketstreamserver.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+/*
+ * domainsocket
+ */
+func UnixDomainSocketStreamServer() {
+	path := filepath.Join(os.TempDir(), "bench-unixdomainsocket-sample")
+	os.Remove(path) // 定石
+	listener, err := net.Listen("unix", path)
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, _ := listener.Accept()
+		go func() {
+			request, err := http.ReadRequest(bufio.NewReader(conn))
+			if err != nil {
+				panic(err)
+			}
+			_, err = httputil.DumpRequest(request, true)
+			if err != nil {
+				panic(err)
+			}
+			response := http.Response{
+				StatusCode: 200,
+				ProtoMajor: 1,
+				ProtoMinor: 0,
+				Body: ioutil.NopCloser(
+					strings.NewReader("Hello world!\n")),
+			}
+			response.Write(conn)
+			conn.Close()
+		}()
+	}
+}
