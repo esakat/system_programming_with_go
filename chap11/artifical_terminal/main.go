2e2aa71887bad52e8b0406d4c9bcb2a996847418
esakat <esaka.tom@gmail.com>
2018-04-15 23:53:02 +0900

    add artifical_terminal

---
+++ chap11/artifical_terminal/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"os"
+	"os/exec"
+
+	"github.com/kr/pty"
+)
+
+func main() {
+	cmd := exec.Command("./check")
+	stdpty, stdtty, _ := pty.Open()
+	defer stdtty.Close()
+	cmd.Stdin = stdpty
+	cmd.Stdout = stdpty
+	errpty, errtty, _ := pty.Open()
+	defer errtty.Close()
+	cmd.Stderr = errpty
+	go func() {
+		io.Copy(os.Stdout, stdpty)
+	}()
+	go func() {
+		io.Copy(os.Stderr, errpty)
+	}()
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+}
