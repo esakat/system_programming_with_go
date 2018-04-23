65b77b2fe22c7a8665a3a00fac92886dc0508e50
esakat <esaka.tom@gmail.com>
2018-04-24 00:33:21 +0900

    add send_signal

---
+++ chap12/send_signal/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("usage: %s [pid]\n", os.Args[0])
+		return
+	}
+	// 第一引数で指定されたプロセスIDを数値に変換
+	pid, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		panic(err)
+	}
+	// シグナルを送る
+	process.Signal(os.Kill)
+	// Killの場合は次のショートカットも利用可能
+	process.Kill()
+}
