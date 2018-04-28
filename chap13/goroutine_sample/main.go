79f1af656c62e1edfcad954c7067146ae08d6395
esakat <esaka.tom@gmail.com>
2018-04-28 23:05:50 +0900

    add goroutine_sample

---
+++ chap13/goroutine_sample/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sub1(c int) {
+	fmt.Println("share by arguments:", c*c)
+}
+
+func main() {
+	// 引数渡し
+	go sub1(10)
+
+	// クローンのキャプチャ渡し
+	c := 20
+	go func() {
+		fmt.Println("share by capture", c*c)
+	}()
+	time.Sleep(time.Second)
+}
